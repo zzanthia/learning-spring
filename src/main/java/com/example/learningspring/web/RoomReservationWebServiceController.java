332520ba33ec393541b4eaa7780e6839c4e23275
zzanthia <zanthia@tut.by>
2020-08-01 23:58:16 +0300

    Learning Spring with Spring Boot: 5. RestController

---
+++ src/main/java/com/example/learningspring/web/RoomReservationWebServiceController.java
@@ -0,0 +1,29 @@
+package com.example.learningspring.web;
+
+import com.example.learningspring.business.domain.RoomReservation;
+import com.example.learningspring.business.service.ReservationService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Date;
+import java.util.List;
+
+@RestController
+@RequestMapping("api/reservations")
+public class RoomReservationWebServiceController {
+    private final ReservationService reservationService;
+
+    @Autowired
+    public RoomReservationWebServiceController(ReservationService reservationService) {
+        this.reservationService = reservationService;
+    }
+
+    @GetMapping
+    public List<RoomReservation> getRoomReservations(@RequestParam(name="date", required = false) String dateString) {
+        Date date = DateUtils.createDateFromDateString(dateString);
+        return reservationService.getRoomReservationsForDate(date);
+    }
+}
